d00b40a5ae86b5febaa8039f49e7643754090c6c
Yehun <97829817+yhyh4420@users.noreply.github.com>
2024-11-12 12:31:36 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,10 +1,10 @@
 N, B = input().split()
 B = int(B)
+
 num_list = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 answer = 0
-for i in range(len(N)):
-    a = num_list.index(N[i])
-    answer_i = a*(B**(len(N)-i-1))
-    answer += answer_i
-    
+for i, a in enumerate(N):
+    k = (num_list.index(a))*(B**(len(N)-i-1))
+    answer += k
+
 print(answer)
