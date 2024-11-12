b93115f25d52f03b518dfc1c89e6f3104995b2b2
Yehun <97829817+yhyh4420@users.noreply.github.com>
2024-11-12 12:13:19 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,18 +1,19 @@
 N = int(input())
-paper = []
 S = [[0]*100 for i in range(100)]
+paper = []
 
 for _ in range(N):
     row = list(map(int, input().split()))
     paper.append(row)
-    
+
 for i in range(N):
     for j in range(10):
         for k in range(10):
             S[paper[i][0] + j][paper[i][1] + k] = 1
+
 answer = 0
 for i in range(100):
     for j in range(100):
         answer += S[i][j]
-        
+
 print(answer)
