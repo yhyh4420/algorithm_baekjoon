a71cdc9eea187ae55b31e0d379ee389b1a069206
Yehun <yhyh4420@naver.com>
2025-03-28 07:33:57 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,3 @@
-a,b,c = map(int,input().split())
+A, B, C = map(int, input().split())
 
-def devide(a,b):
-    if b==1:
-        return a%c
-    else:
-        temp = devide(a,b//2)
-        if b%2==0:
-            return temp*temp%c
-        else:
-            return temp*temp*a%c
-
-print(devide(a,b))
+print(pow(A,B,C))
