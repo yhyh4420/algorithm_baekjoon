1f78b13c6c8f22d4e42095405d44bc738b1726a7
Yehun <yhyh4420@naver.com>
2025-08-30 10:52:22 +0900

    [level 2] Title: 점 찍기, Time: 16.52 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(long k, long d) {
+        long answer = 0;
+
+        for(long x = 0; x <= d; x += k){
+            long maxY = (long) Math.sqrt(d*d - x*x); 
+            answer += maxY / k + 1;
+        }
+        
+        return answer;
+    }
+}
