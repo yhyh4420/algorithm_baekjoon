63f2659a2759e27bbf954a6ff8a691df28dff405
장예훈 <yhyh4420@naver.com>
2026-01-08 22:45:43 +0900

    [Bronze V] Title: N 찍기, Time: 804 ms, Memory: 30028 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++){
+            System.out.println(i+1);
+        }
+    }
+}
